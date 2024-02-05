b24257fb93714473113dde7a36122f49009162ff
ujamir127@gmail.com <ujamir127@gmail.com>
2024-02-05 18:56:03 +0600

    Add project files.

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Hubs
+{
+	public class ChatHub:Hub
+	{
+		public async Task Share(string user, string message)
+		{
+			//await Clients.All.SendAsync("receive", user, message);
+
+			await Clients.Caller.SendAsync("receive", "SELF", message);
+			await Clients.Others.SendAsync("receive", user, message);
+		}
+
+	}
+}
---
+++ wwwroot/js/chat.js
@@ -0,0 +1,36 @@
+﻿/// <reference path="../microsoft/signalr/dist/browser/signalr.js" />
+
+
+
+
+var username = prompt("enter your name", "DITC");
+
+var hubBuilder = new signalR.HubConnectionBuilder();
+var connection = hubBuilder.withUrl("/chat").build();
+
+
+
+
+connection.on("receive", function (user, message) {
+    var li = document.createElement("li");
+
+
+    li.textContent = `${user} : ${message}`;
+
+    $('#chatLog').append(li);
+
+
+});
+
+
+
+connection.start();
+
+
+$('#btnSend').on('click', function () {
+
+    var message = $('#txtInput').val();
+
+    connection.invoke("Share", username, message);
+    $('#txtInput').val(null);
+})
